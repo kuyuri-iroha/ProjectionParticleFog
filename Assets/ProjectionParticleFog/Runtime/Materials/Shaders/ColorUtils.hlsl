e08e8df2330e9d1eded12a6b590c3b8cd38ba268
kuyuri-iroha <kuyuri.iroha@gmail.com>
2022-09-03 16:11:45 +0900

    LowCut用閾値の追加

--- Assets/ProjectionParticleFog/Runtime/Materials/Shaders/ColorUtils.hlsl
+++ Assets/ProjectionParticleFog/Runtime/Materials/Shaders/ColorUtils.hlsl
@@ -12,4 +12,12 @@ void ChangeColorIntensity_float(float3 color, float intensity, out float3 newCol
     newColor += color * (intensity - floor(intensity));
 }
 
+void ColorLowCutFilter_float(float4 color, float4 threshold, out float4 newColor)
+{
+    for(int i = 0; i < 4; i++)
+    {
+        newColor[i] = color[i] < threshold[i] ? 0.0f : color[i];
+    }
+}
+
 #endif //COLORUTILS_INCLUDE
